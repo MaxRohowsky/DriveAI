4824305a0ca3804aeaefd9a092f193b1b2c6491b
codewmax <68344959+codewmax@users.noreply.github.com>
2021-05-11 12:17:58 +0200

    Update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SCREEN_WIDTH = 1244
 SCREEN_HEIGHT = 1016
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-TRACK = pygame.image.load(os.path.join("Assets", "track.png"))
+TRACK = pygame.image.load(os.path.join("Assets", "trackV2.png"))
 
 
 class Car(pygame.sprite.Sprite):
